23353aa14d93701446f9f6a532d46335247c764f
Ender <zengande@outlook.com>
2024-04-30 15:50:16 +0800

    chore: use https

--- .umirc.prod.ts
+++ .umirc.prod.ts
@@ -4,12 +4,12 @@ export default defineConfig({
     define: {
         SHOW_OIDC_LOGGING: false,
 
-        ODIC_AUTHORITY: 'http://110.41.18.47:8081',
+        ODIC_AUTHORITY: 'https://110.41.18.47:8081',
         ODIC_CLIENT_ID: 'openauthing-admin',
         ODIC_CLIENT_SECRET: '',
 
-        USER_PROFILE_URL: 'http://110.41.18.47:8081/settings/profile',
+        USER_PROFILE_URL: 'https://110.41.18.47:8081/settings/profile',
 
-        ADMIN_API_BASE_URL: 'http://110.41.18.47:8082',
+        ADMIN_API_BASE_URL: 'https://110.41.18.47:8082',
     },
 });
